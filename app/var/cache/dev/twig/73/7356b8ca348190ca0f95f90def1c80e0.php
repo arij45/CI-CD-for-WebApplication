6026d00ec608964580ed934b9f7d0c97b0ed872b
arij45 <arij.kortas@enetcom.u-sfax.tn>
2023-04-16 02:01:35 -0700

    commit

--- app/var/cache/dev/twig/73/7356b8ca348190ca0f95f90def1c80e0.php
+++ app/var/cache/dev/twig/73/7356b8ca348190ca0f95f90def1c80e0.php
@@ -24,15 +24,10 @@ class __TwigTemplate_073cf5d7228fbd760cf120a9b5e7fe16 extends Template
 
         $this->source = $this->getSourceContext();
 
-        $this->blocks = [
-            'body' => [$this, 'block_body'],
-        ];
-    }
+        $this->parent = false;
 
-    protected function doGetParent(array $context)
-    {
-        // line 1
-        return "base.html.twig";
+        $this->blocks = [
+        ];
     }
 
     protected function doDisplay(array $context, array $blocks = [])
@@ -44,45 +39,23 @@ class __TwigTemplate_073cf5d7228fbd760cf120a9b5e7fe16 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "index.html.twig"));
 
-        $this->parent = $this->loadTemplate("base.html.twig", "index.html.twig", 1);
-        $this->parent->display($context, array_merge($this->blocks, $blocks));
-        
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
-
-        
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
-    }
-
-    // line 3
-    public function block_body($context, array $blocks = [])
-    {
-        $macros = $this->macros;
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
-
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
-
-        // line 4
-        echo "        <header>
+        // line 1
+        echo "
+        <header>
           <div class=\"view\" style=\"background-image: url('https://www.polytecsousse.tn/wp-content/themes/edubin/assets/images/header.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;\">
             <div class=\"mask rgba-gradient align-items-center\">
                 <div class=\"row\">
                   <div style=\"margin-left: 50%;\" class=\"col-md-6 white-text text-center text-md-left mt-xl-5 mb-5 wow fadeInLeft\" data-wow-delay=\"0.3s\">
                     <h1 class=\"h1-responsive font-weight-bold mt-sm-5\" style=\"color: #07294d\">GoFormations </h1>
                     <hr class=\"hr-light\">
-<<<<<<< HEAD
-                    <h5 class=\"mb-4\" style=\"color:white;\">lokijh d'apprentissage 100% en ligne. Inscrivez-vous pour découvrir les différents cours proposés et bénéficier d'une expérience e-learning enrichissante ! Si vous êtes déjà enregistrés, connectez vous.</h5>
-=======
-                    <h5 class=\"mb-4\" style=\"color:white;\">jkkkkjnoll,,prentissage 100% en ligne. Inscrivez-vous pour découvrir les différents cours proposés et bénéficier d'une expérience e-learning enrichissante ! Si vous êtes déjà enregistrés, connectez vous.</h5>
->>>>>>> 4e256e7d00b7aea78697035aa8b5d7e6dd250aa0
+                    <h5 class=\"mb-4\" style=\"color:white;\">site d'apprentisage 100% en ligne. Inscrivez-vous pour découvrir les différents cours proposés et bénéficier d'une expérience e-learning enrichissante ! Si vous êtes déjà enregistrés, connectez vous.</h5>
+
                     <a href=\"";
-        // line 16
+        // line 11
         echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("login");
         echo "\" style=\"color:rgb(100, 212, 231);\" class=\"btn btn-white\"><b>Se connecter</b></a>
                     <a href=\"";
-        // line 17
+        // line 12
         echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_register");
         echo "\" style=\"color:rgb(100, 212, 231);\" class=\"btn btn-white\"><b>S'inscrire</b></a>
                    <a href=\"/suggestions/new2\" style=\"color:rgb(100, 212, 231);\" class=\"btn btn-outline-white\"><b>Proposez des formations de vos choix</b></a>
@@ -104,12 +77,12 @@ class __TwigTemplate_073cf5d7228fbd760cf120a9b5e7fe16 extends Template
                 
             }
         </style>
-   ";
-        
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
+";
         
         $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
     }
 
@@ -125,14 +98,12 @@ class __TwigTemplate_073cf5d7228fbd760cf120a9b5e7fe16 extends Template
 
     public function getDebugInfo()
     {
-        return array (  86 => 17,  82 => 16,  68 => 4,  58 => 3,  35 => 1,);
+        return array (  59 => 12,  55 => 11,  43 => 1,);
     }
 
     public function getSourceContext()
     {
-        return new Source("{% extends 'base.html.twig' %}
-
-    {% block body %}
+        return new Source("
         <header>
           <div class=\"view\" style=\"background-image: url('https://www.polytecsousse.tn/wp-content/themes/edubin/assets/images/header.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;\">
             <div class=\"mask rgba-gradient align-items-center\">
@@ -140,11 +111,8 @@ class __TwigTemplate_073cf5d7228fbd760cf120a9b5e7fe16 extends Template
                   <div style=\"margin-left: 50%;\" class=\"col-md-6 white-text text-center text-md-left mt-xl-5 mb-5 wow fadeInLeft\" data-wow-delay=\"0.3s\">
                     <h1 class=\"h1-responsive font-weight-bold mt-sm-5\" style=\"color: #07294d\">GoFormations </h1>
                     <hr class=\"hr-light\">
-<<<<<<< HEAD
-                    <h5 class=\"mb-4\" style=\"color:white;\">lokijh d'apprentissage 100% en ligne. Inscrivez-vous pour découvrir les différents cours proposés et bénéficier d'une expérience e-learning enrichissante ! Si vous êtes déjà enregistrés, connectez vous.</h5>
-=======
-                    <h5 class=\"mb-4\" style=\"color:white;\">jkkkkjnoll,,prentissage 100% en ligne. Inscrivez-vous pour découvrir les différents cours proposés et bénéficier d'une expérience e-learning enrichissante ! Si vous êtes déjà enregistrés, connectez vous.</h5>
->>>>>>> 4e256e7d00b7aea78697035aa8b5d7e6dd250aa0
+                    <h5 class=\"mb-4\" style=\"color:white;\">site d'apprentisage 100% en ligne. Inscrivez-vous pour découvrir les différents cours proposés et bénéficier d'une expérience e-learning enrichissante ! Si vous êtes déjà enregistrés, connectez vous.</h5>
+
                     <a href=\"{{ path('login') }}\" style=\"color:rgb(100, 212, 231);\" class=\"btn btn-white\"><b>Se connecter</b></a>
                     <a href=\"{{ path('app_register') }}\" style=\"color:rgb(100, 212, 231);\" class=\"btn btn-white\"><b>S'inscrire</b></a>
                    <a href=\"/suggestions/new2\" style=\"color:rgb(100, 212, 231);\" class=\"btn btn-outline-white\"><b>Proposez des formations de vos choix</b></a>
@@ -166,7 +134,6 @@ class __TwigTemplate_073cf5d7228fbd760cf120a9b5e7fe16 extends Template
                 
             }
         </style>
-   {% endblock %}
 ", "index.html.twig", "/var/www/app/templates/index.html.twig");
     }
 }
